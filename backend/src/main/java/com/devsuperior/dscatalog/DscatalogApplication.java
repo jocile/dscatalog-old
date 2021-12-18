6efccc8af4f46fe294d01deb0e51aa3a470343fb
jocile <jocile@gmail.com>
2021-12-18 14:41:05 -0300

    Documentação para o OpenApi usando swagger anot.

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -1,17 +1,21 @@
 package com.devsuperior.dscatalog;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "DevSuperior Catálogo", version = "0.1.0", description = "App de catálogo com CRUD e listagem de produtos"))
+@OpenAPIDefinition(
+  info = @Info(
+    title = "DevSuperior Catálogo",
+    version = "0.1.0",
+    description = "App de catálogo com CRUD e listagem de produtos"
+  )
+)
 public class DscatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DscatalogApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DscatalogApplication.class, args);
+  }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.List;
-
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-
   @Autowired
   private CategoryService service;
 
   @GetMapping
+  @Operation(
+    summary = "Lista as categorias",
+    description = "Lista todas as categorias",
+    tags = { "Categorias" },
+    responses = {
+      @ApiResponse(
+        description = "Sucesso",
+        responseCode = "200",
+        content = @Content(
+          mediaType = "application/json",
+          schema = @Schema(implementation = CategoryDTO.class)
+        )
+      ),
+    }
+  )
   public ResponseEntity<List<CategoryDTO>> findAll() {
     List<CategoryDTO> list = service.findAll();
     return ResponseEntity.ok().body(list);
