0d18aaeb33ff3d53f599b7b0395ee6e9e8e56edf
jocile <jocile@gmail.com>
2022-02-19 10:56:54 -0300

    Product is deleted when ID doesnt exist with test

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -15,12 +16,20 @@ public class ProductRepositoryTests {
   @Test
   public void deleteShouldDeleteObjectWhenIdExists() {
     long existingId = 1L;
-
     repository.deleteById(existingId);
-
     repository.findById(existingId);
-
     Optional<Product> result = repository.findById(existingId);
     Assertions.assertFalse(result.isPresent());
+  }
+
+  @Test
+  public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+    long nonExistingId = 1000L;
+    Assertions.assertThrows(
+      EmptyResultDataAccessException.class,
+      () -> {
+        repository.deleteById(nonExistingId);
+      }
+    );
   }
 }
